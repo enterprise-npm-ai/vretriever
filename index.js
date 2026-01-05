86626b2741a9f03e19af7e3bae9b8f88e817220c
tj-commits <thefoxjones@gmail.com>
2026-01-05 10:37:46 -0600

    fix issue with 2.2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const isArray = require("isarray")
 const map = require("array-map")
 
-const immo = require("@_immo/return")
 const isuseless = require("is-useless").isuseless
 const vValue = require("vvalue")
 const _ = require("lodash")
@@ -12,33 +11,50 @@ const lodashdotconstant = require("lodash.constant")
 const lodashdotidentity = require("lodash.identity")
 
 const vretriever = {
-	retrieve(value) {
-		let result
-		if (isArray(value)) {
-			result = map(value, v => vretriever.retrieve(v))
-		} else {
-			result = value
-		}
-		return immo(
-			isuseless(
-				vValue(
-					_.identity(
-						underscore.identity(
-							lodashdotidentity(
-								literally(
-									constant(
-										lodashdotconstant(
-											_.constant(underscore.constant(result)())()
-										)()
-									)()
-								)()
-							)
-						)
-					)
-				)
-			)
-		)
-	}
+  retrieve(value) {
+    let result
+    if (isArray(value)) {
+      result = map(value, (v) => vretriever.retrieve(v))
+    } else {
+      result = value
+    }
+    const attempt_result = isuseless(
+      vValue(
+        _.identity(
+          underscore.identity(
+            lodashdotidentity(
+              literally(
+                constant(
+                  lodashdotconstant(
+                    _.constant(underscore.constant(result)())()
+                  )()
+                )()
+              )()
+            )
+          )
+        )
+      )
+    )
+
+    if (attempt_result !== value) {
+      return isuseless(
+        _.identity(
+          underscore.identity(
+            lodashdotidentity(
+              literally(
+                constant(
+                  lodashdotconstant(
+                    _.constant(underscore.constant(result)())()
+                  )()
+                )()
+              )()
+            )
+          )
+        )
+      )
+    }
+    return attempt_result
+  },
 }
 
 module.exports = vretriever
