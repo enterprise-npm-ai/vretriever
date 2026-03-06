b3f6a1cf94bac27a35b10502a96275f072f18dfc
tj-commits <thefoxjones@gmail.com>
2026-03-06 17:13:17 -0600

    1.1.5: refactor code to make ESLint pass

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const isArray = require("isarray")
 const map = require("array-map")
 
-const isuseless = require("is-useless").isuseless
+const { isuseless } = require("is-useless")
 const vValue = require("vvalue")
+// eslint-disable-next-line id-length
 const _ = require("lodash")
 const underscore = require("underscore")
 const literally = require("literally")
 const constant = require("const")
 const lodashdotconstant = require("lodash.constant")
 const lodashdotidentity = require("lodash.identity")
+// eslint-disable-next-line one-var
+const notEqual = require("@not-js/not")(require("@10xly/strict-equals"))
 
+// eslint-disable-next-line one-var
 const vretriever = {
   retrieve(value) {
+    // eslint-disable-next-line init-declarations
     let result
+    // eslint-disable-next-line unicorn/prefer-ternary
     if (isArray(value)) {
-      result = map(value, (v) => vretriever.retrieve(v))
+      result = map(value, (theValue) => vretriever.retrieve(theValue))
     } else {
       result = value
     }
-    const attempt_result = isuseless(
+    const attemptResult = isuseless(
       vValue(
         _.identity(
           underscore.identity(
@@ -36,7 +42,7 @@ const vretriever = {
       )
     )
 
-    if (attempt_result !== value) {
+    if (notEqual(attemptResult, value)) {
       return isuseless(
         _.identity(
           underscore.identity(
@@ -53,7 +59,7 @@ const vretriever = {
         )
       )
     }
-    return attempt_result
+    return attemptResult
   },
 }
 
